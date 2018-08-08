b0881feb39e47be089c1fbf9891624f2700f8aa8
Quentin LEONE <qmip@hotmail.fr>
2018-08-08 22:12:24 +0200

    update

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -58,7 +58,22 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    -- awful.layout.suit.floating,
+    -- awful.layout.suit.tile,
+    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile.bottom,
+    -- awful.layout.suit.tile.top,
     awful.layout.suit.fair,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.max,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.corner.nw,
+    -- awful.layout.suit.corner.ne,
+    -- awful.layout.suit.corner.sw,
+    -- awful.layout.suit.corner.se,
 }
 -- }}}
 
@@ -205,7 +220,6 @@ awful.screen.connect_for_each_screen(function(s)
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
@@ -318,6 +332,9 @@ globalkeys = gears.table.join(
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end,
               {description = "show the menubar", group = "launcher"}),
+    -- Lock screen
+    awful.key({ modkey }, "F12", function() awful.util.spawn("slock") end,
+              {description = "lock screen", group = "launcher"}),
     -- Open firefox
     awful.key({ modkey }, "f", function() awful.util.spawn("firefox") end,
               {description = "open firefox", group = "launcher"})
