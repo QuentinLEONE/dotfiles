c6ca6765a90ed72960ceace60a8bd098c5b02a5f
Quentin LEONE <qmip@hotmail.fr>
2018-11-25 22:54:23 +0100

    update: change font Oxygen Mono to Fira Mono

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Oxygen Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Mono:style=Regular:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
