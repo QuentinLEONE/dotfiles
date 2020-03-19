ee1eed9eec387f559430d6ee6fc135c64433bf35
Quentin LEONE <qmip@hotmail.fr>
2020-03-19 14:42:04 +0100

    fix config fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,8 @@ set -x EDITOR vim
 set fish_cursor_insert
 source ~/.config/fish/z.fish
 
-if status is_login
-	if test -z "$DISPLAY" -a $XDG_VTNR > 0
-		exec startx keeptty
+if status is-login
+	if test -z "$DISPLAY" -a $XDG_VTNR = 1
+		exec startx -- -keeptty
 	end
 end
